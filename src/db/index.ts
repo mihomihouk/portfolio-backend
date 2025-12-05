5a1cf6aab6f8b7bbefa61277dec65cc2a8376e0e
Miho Inagaki <mihoinagaki@Mihos-Air.mynet>
2025-12-05 22:58:18 +0000

    replace mysql with postgreSQL

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,8 @@
-import mysql from 'mysql2/promise'
 import dotenv from 'dotenv';
+import {Pool} from 'pg'
 
 dotenv.config();
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST || 'locahost',
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME ||''
+export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
 })
--- src/db/log.ts
+++ src/db/log.ts
@@ -1,40 +1,57 @@
-import {db} from './index'
+import { db } from './index';
 
-export function logEvent({event, path, referrer, userAgent, ip}:{
-    event: string,
-    path: string,
-    referrer:string,
-    userAgent:string,
-    ip?: string | undefined
-}){
-    return db.execute(
-        'INSERT INTO logs (event, path, referrer, user_agent, ip) VALUES (?,?,?,?,?)',
-        [event, path, referrer, userAgent, ip]
-      );
+export function logEvent({
+  event,
+  path,
+  referrer,
+  userAgent,
+  ip,
+}: {
+  event: string;
+  path: string;
+  referrer: string;
+  userAgent: string;
+  ip?: string | undefined;
+}) {
+  return db.query(
+    `
+    INSERT INTO logs (event, path, referrer, user_agent, ip)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING id
+    `,
+    [event, path, referrer, userAgent, ip || null]
+  );
 }
 
-export function getVisitorCount({daysAgo = 30}:{daysAgo?:number|undefined}){
-    return db.execute(`
-        SELECT 
-            COUNT(id) as visits, 
-            DATE(created_at) as date 
-        FROM logs
-        WHERE event = 'page_view' 
-            AND path = '/' 
-            AND created_at >= DATE_SUB(NOW(), INTERVAL ? DAY)
-        GROUP BY DATE(created_at)
-        ;
-        `, [daysAgo])
+export function getVisitorCount({ daysAgo = 30 }: { daysAgo?: number | undefined }) {
+  return db.query(
+    `
+    SELECT 
+        COUNT(id) AS visits,
+        DATE(created_at) AS date
+    FROM logs
+    WHERE event = 'page_view'
+      AND path = '/'
+      AND created_at >= NOW() - INTERVAL '$1 days'
+    GROUP BY DATE(created_at)
+    ORDER BY DATE(created_at)
+    `,
+    [daysAgo]
+  );
 }
 
-export function getPagePopularity({daysAgo = 30}:{daysAgo?:number|undefined}){
-    return db.execute(`
-        SELECT
-            COUNT(id) as visits,
-            path as page
-        FROM logs
-        WHERE event = 'page_view'
-            AND created_at >= DATE_SUB(NOW(), INTERVAL ? DAY)
-        GROUP BY path
-    `, [daysAgo])
+export function getPagePopularity({ daysAgo = 30 }: { daysAgo?: number | undefined }) {
+  return db.query(
+    `
+    SELECT
+        COUNT(id) AS visits,
+        path AS page
+    FROM logs
+    WHERE event = 'page_view'
+      AND created_at >= NOW() - INTERVAL '$1 days'
+    GROUP BY path
+    ORDER BY visits DESC
+    `,
+    [daysAgo]
+  );
 }
