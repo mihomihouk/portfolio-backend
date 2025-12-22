c7c817d71e8e6ea58c141afaac15158e91e881e7
Miho Inagaki <93969412+mihomihouk@users.noreply.github.com>
2025-12-22 15:29:52 +0000

    Add rate limiting to /api/log endpoint (#15)
    * Install rate limit library
    
    * Add rate limit middleware to log route
    
    * Update README with info about rate-limiting

--- src/routes/log.route.ts
+++ src/routes/log.route.ts
@@ -2,9 +2,10 @@ import { Router } from 'express'
 import { logUserActivity } from '../controllers/log'
 import { validateBody } from '../middlewares/validate.middleware'
 import { LogEventDTO } from '../dtos/log.dto'
+import { logRateLimiter } from '../middlewares/rate-limit'
 
 const router = Router()
 
-router.post('/', validateBody(LogEventDTO), logUserActivity)
+router.post('/', logRateLimiter, validateBody(LogEventDTO), logUserActivity)
 
 export default router
